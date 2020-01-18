19f14cfa833f4e8ec81948bf472a93bae730489c
syoukou <fatman01221087@gmail.com>
2020-01-18 19:23:42 +0900

    [MEMO1.0-007] メモが空の場合の表示を修正しました #2

--- app/src/main/java/com/example/e01_memo/ui/main/MainFragment.java
+++ app/src/main/java/com/example/e01_memo/ui/main/MainFragment.java
@@ -225,7 +225,7 @@ public class MainFragment extends BaseFragment implements MainContract.MainView 
         recyclerView.setVisibility(View.GONE);
         emptyText.setVisibility(View.VISIBLE);
         if (MyApplication.selectNavigationItem != Constant.NavigationItem.DELETE) {
-            emptyText.setText("メモがありません。");
+            emptyText.setText(R.string.not_memo);
         } else {
             emptyText.setText("空です");
         }
